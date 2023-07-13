dbb1ff80ab41138299bf92910091be5817017d91
RennieChien <133186945+RennieChien@users.noreply.github.com>
2023-07-13 17:04:43 +0800

    Seventh commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFF0000, 0xAAAAAA, 1.0);
+		const ambient = new THREE.HemisphereLight(0xFF0000, 0xAAAAAA, 0.6);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
