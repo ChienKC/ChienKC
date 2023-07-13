005d5956be4c402be41c41ffa752901b19c2fe83
RennieChien <133186945+RennieChien@users.noreply.github.com>
2023-07-13 22:22:07 +0800

    24th

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ class App{
                     const treasureBox = gltf.scene;
                     // Adjust the position and rotation of the treasure box as needed
                     treasureBox.position.set(1, 30, 10); //1,0.5,2
-                    //treasureBox.rotation.y = Math.PI / 2;
+                    treasureBox.rotation.set = (0, 0, 0);
         
                     // Add the treasure box to the college scene
                     college.add(treasureBox);
