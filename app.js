f5b8bbc9d74da4ebccbc70cf1991d3039e7eb33d
RennieChien <133186945+RennieChien@users.noreply.github.com>
2023-07-14 12:13:34 +0800

    41st

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ class App{
                     function (gltf) {
                     const treasureBox = gltf.scene;
                     // Adjust the position and rotation of the treasure box as needed
-                    treasureBox.position.set(1, -10+3000, -1200); //1,0.5,2,y,z,x
+                    treasureBox.position.set(1, -10+1500, -1200); //1,0.5,2,y,z,x
                     treasureBox.rotation.x = Math.PI;
                     treasureBox.rotation.y = Math.PI;
                     treasureBox.scale.set(0.25, 0.25, 0.25);
